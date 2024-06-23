4be6ba1c4ab288e129f7578f3f00815cdf90c0bb
TOPTEN Agency <111628562+toptenagency@users.noreply.github.com>
2024-06-23 03:29:08 -0500

    footer adjustments

--- components/sections/home-footer.tsx
+++ components/sections/home-footer.tsx
@@ -2,43 +2,13 @@ import {
   PuzzlePieceIcon,
   CloudArrowDownIcon,
 } from "@heroicons/react/24/outline";
+import ServicesFooter from "./services-footer";
+
 export default function Homefooter() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-16 md:pt-0 lg:pb-32 sm:pt-0">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-          <div className="max-w-xl lg:max-w-lg">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Solicita una consultoría gratuita hoy.
-            </h2>
-            <p className="mt-4 text-md leading-8 text-gray-400">
-              Sabemos que cada negocio es único y tiene diferentes necesidades y
-              objetivos. Por eso, ofrecemos un servicio adicional de consultoría
-              en la que platicaremos las dudas que puedas tener con tu próxima
-              estrategia de marketing digital.
-            </p>
-            <div className="mt-6 flex max-w-md gap-x-4">
-              <label htmlFor="email-address" className="sr-only">
-                {" "}
-                Email{" "}
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                placeholder="Enter your email"
-              />
-              <button
-                type="submit"
-                className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-grey-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
-              >
-                Solicitar
-              </button>
-            </div>
-          </div>
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
             <div className="flex flex-col items-start">
               <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
@@ -67,10 +37,41 @@ export default function Homefooter() {
               </dt>
               <dd className="mt-2 leading-7 text-gray-400">
                 Respaldo seguro en nuestro servidor privado y acceso remoto a
-                todos tus archivos 24/7.{" "}
+                todos tus archivos 24/7.
               </dd>
             </div>
           </dl>
+          <div className="max-w-xl lg:max-w-lg lg:ml-20">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Solicita una consultoría gratuita hoy.
+            </h2>
+            <p className="mt-4 text-md leading-8 text-gray-400">
+              Sabemos que cada negocio es único y tiene diferentes necesidades y
+              objetivos. Por eso, ofrecemos un servicio adicional de consultoría
+              en la que platicaremos las dudas que puedas tener con tu próxima
+              estrategia de marketing digital.
+            </p>
+            <div className="mt-6 flex max-w-md gap-x-4">
+              <label htmlFor="email-address" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+                placeholder="Enter your email"
+              />
+              <button
+                type="submit"
+                className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-grey-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+              >
+                Solicitar
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <div
@@ -86,6 +87,7 @@ export default function Homefooter() {
           }}
         />
       </div>
+      <ServicesFooter />
     </div>
   );
 }
